366436005d67274101881445bd1c9a2cd8455334
EsauLu <1165050143@qq.com>
2016-10-09 16:08:59 +0800

    filr

---
+++ coursetable/src/main/java/com/fatcat/coursetable/widget/CourseWidgetProvider.java
@@ -0,0 +1,15 @@
+package com.fatcat.coursetable.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * 窗口小部件
+ *
+ * Created by EsauLu on 2016-10-09.
+ */
+
+public class CourseWidgetProvider extends AppWidgetProvider {
+
+
+
+}
